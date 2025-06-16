2bb885a5388f129a2bc5242cafa801ed8f23546c
Rogielyn Solomon <solomonrogielyn0724@gmail.com>
2025-06-16 22:42:08 +0000

    I want you to use ios font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,8 +18,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Merriweather', 'serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
